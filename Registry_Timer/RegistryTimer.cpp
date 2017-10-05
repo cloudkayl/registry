df17eab4e2422fb8e93822888a598231386c4329
Ayush Rout <heroayush@gmail.com>
2017-10-05 10:46:14 -0400

    Updated amba

--- Registry_Timer/RegistryTimer.cpp
+++ Registry_Timer/RegistryTimer.cpp
@@ -8,14 +8,14 @@ int main() {
 	//initial value of the amba register
 	printf("After initialization, amba = 0x%X\n",amba);
 	//set 'Mode bit' and 'Enable bit' high
-	amba = 0xC0; 
+	amba = amba|0xC0; 
 	//you need to change this line!
 	printf("After mode and enable bits set, amba = 0x%X\n",amba);
 	//alsoset the 'prescale' bits so 'clock divide by' is 16
-	amba = 0x8; //you need to change this line!
+	amba = amba|~0x3B; //you need to change this line!
 	printf("After setting prescale bits, amba = 0x%X\n",amba);
 	//clear the 'Mode bit', leaving other bits alone
-	amba = amba & 0x80;
+	amba = amba & ~0x43;
 	//you need to change this line!
 	printf("After clearing the mode bit, amba = 0x%X\n",amba);
 	return 0;
